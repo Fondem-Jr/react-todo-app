6d637288ecc59bd2e8547ccf0c90e3e04c1cd7a8
Fondem Junior <fondemjunior@yahoo.com>
2021-11-30 17:06:54 +0100

    Add react router

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TodoContainer from './functionBased/components/TodoContainer';
 import "./functionBased/App.css"
+import { BrowserRouter as Router } from "react-router-dom"
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <TodoContainer  />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <React.StrictMode>
+      <Router>
+        <TodoContainer />
+      </Router>
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
